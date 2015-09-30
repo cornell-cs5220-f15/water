4c24c703bc8fd4234a571bc4955768fdb1c8dd77
David Bindel <bindel@cs.cornell.edu>
2015-09-30 09:34:25 -0400

    Updates to Makefile and initial comments.

--- Makefile
+++ Makefile
@@ -6,5 +6,11 @@ run: shallow
 	convert test.pgm test.png
 	convert test2.pgm test2.png
 
+shallow.pdf: shallow.md
+	pandoc $< -o $@
+
+shallow.md: shallow.cc
+	ldoc $< -o $@
+
 clean:
 	rm -f shallow test.pgm test2.pgm test.png test2.png
--- shallow.cc
+++ shallow.cc
@@ -1,10 +1,23 @@
-/*
- * Jiang-Tadmor centered difference scheme with MinMod limiter,
- * applied to the shallow water equations (St. Venant).
- *
- * See Moler's "Experiments in MATLAB"
- * [moler]: https://www.mathworks.com/moler/exm/chapters/water.pdf
+//ldoc on
+/**
+ * % Shallow water simulation
+ * % David Bindel
+ * % 2015-09-30
+ * 
+ * This code implements the [Jiang-Tadmor centered finite volume
+ * scheme][jt] for conservation law PDEs in 2D, and uses the shallow
+ * water wave equations as an example.  As implemented, the code is
+ * serial, and not particularly carefully tuned.  Your goal: make the
+ * code run fast on the cluster, using OpenMP and (ideally) taking
+ * advantage of the Xeon Phi accelerators.
+ * 
+ * While I have tried not to do anything too obscure, this code does
+ * use some C++ 11 features.  If you want to build on your own
+ * machine, you may need to figure out the flag needed to tell your
+ * compiler that you are using this C++ dialect.
+ * 
  */
+//ldoc off
 
 #include <cstdio>
 #include <cmath>
@@ -16,35 +29,84 @@
 
 using namespace std;
 
-
-/********************************************************************
- * # Define shallow water physics
- *
- * The shallow water equations are a system of conservation equations
- * relating the depth, horizontal momentum, and vertical momentum for
- * each water column in a domain.  We write these equations in the form
- * \[
+//ldoc on
+/**
+ * # Shallow water equations
+ * 
+ * ## Physics picture
+ * 
+ * The shallow water equations are a two-dimensional PDE system
+ * that describes water waves that are very long compared to the
+ * water depth.  It applies even in situations that you might not
+ * think of as "shallow"; for example, tsunami waves are long enough
+ * that they can be modeled using the shallow water equations even
+ * when traveling over mile-deep parts of oceans.
+ * 
+ * The shallow water equations treat water as incompressible and
+ * inviscid, and assume that the horizontal velocity remains constant
+ * in any vertical column of water.  The unknowns at each point are
+ * the water height and the total horizontal momentum in a water
+ * column; the equations describe conservation of mass (fluid is
+ * neither created nor destroyed) and conservation of linear momentum.
+ * We will solve these equations with a numerical method that also
+ * exactly conserves mass and momentum (up to rounding error), though
+ * it only approximately conserves energy.
+ * 
+ * I was inspired to use this system for our assignment by reading the
+ * chapter on [shallow water simulation in MATLAB][exm] from Cleve
+ * Moler's books on "Experiments in MATLAB"; there is also a very readable
+ * [Wikipedia article][wiki] on the shallow water equations.
+ * The basic variables are water height ($h$), and the velocity components
+ * ($u, v$).  We write the governing equations in the form
+ * $$
  *   U_t = F(U)_x + G(U)_y
- * \]
+ * $$
  * where
- * \[
+ * $$
  *   U = \begin{bmatrix} h \\ hu \\ hv \end{bmatrix},
  *   F = \begin{bmatrix} hu \\ h^2 u + gh^2/2 \\ huv \end{bmatrix}
  *   G = \begin{bmatrix} hv \\ huv \\ h^2 v + gh^2/2 \end{bmatrix}
- * \]
+ * $$
+ * The functions $F$ and $G$ are called *fluxes*, and describe how the
+ * conserved quantities (volume and momentum) enter and exit a region
+ * of space.
+ * 
  * Note that we also need a bound on the characteristic wave speeds
  * for the problem in order to ensure that our method doesn't explode;
  * we use this to control the Courant-Friedrichs-Levy (CFL) number
- * relating wave speeds, time steps, and space steps.
+ * relating wave speeds, time steps, and space steps.  For the shallow
+ * water equations, the characteristic wave speed is $\sqrt{g h}$
+ * where $g$ is the gravitational constant and $h$ is the height of the
+ * water; in addition, we have to take into account the velocity of
+ * the underlying flow.
+ * 
+ * [exm]: https://www.mathworks.com/moler/exm/chapters/water.pdf
+ * [wiki]: https://en.wikipedia.org/wiki/Shallow_water_equations
+ * 
+ * ## Implementation
+ * 
+ * Our solver takes advantage of C++ templates to get (potentially)
+ * good performance while keeping a clean abstraction between the
+ * solver code and the details of the physics.  The `Shallow2D`
+ * class specifies the precision of the comptutation (single precision),
+ * the data type used to represent vectors of unknowns and fluxes
+ * (the C++ `std::array`).  We are really only using the class as 
+ * name space; we never create an instance of type `Shallow2D`,
+ * and the `flux` and `wave_speed` functions needed by the solver are
+ * declared as static (and inline, in the hopes of getting the compiler
+ * to optimize for us).
  */
 
 struct Shallow2D {
 
+    // Type parameters for solver
     typedef float real;
     typedef std::array<real,3> vec;
 
-    static constexpr real g = 9.8;  // Gravitational force
+    // Gravitational force (compile time constant)
+    static constexpr real g = 9.8;
 
+    // Compute shallow water fluxes F(U), G(U)
     static void flux(vec& FU, vec& GU, const vec& U) {
         real h = U[0], hu = U[1], hv = U[2];
 
@@ -57,6 +119,7 @@ struct Shallow2D {
         GU[2] = hv*hv/h + (0.5*g)*h*h;
     }
 
+    // Compute shallow water wave spped
     static void wave_speed(real& cx, real& cy, const vec& U) {
         real h = U[0], hu = U[1], hv = U[2];
         real root_gh = sqrt(g * h);
@@ -66,33 +129,50 @@ struct Shallow2D {
 };
 
 
-/********************************************************************
+/**
  * # Jiang-Tadmor central difference scheme
- *
- * Jiang and Tadmor proposed a high-resolution finite difference
+ * 
+ * [Jiang and Tadmor][jt] proposed a high-resolution finite difference
  * scheme for solving hyperbolic PDE systems in two space dimensions.
  * The method is particularly attractive because, unlike many other
  * methods in this space, it does not require that we write any
- * solvers for problems involving F and G with special initial data
- * (so-called Riemann problems), nor even that we compute Jacobians
- * of F and G.  The scheme is still somewhat complicated, partly
- * because of the existence of a "limiter" (required for stability
- * for almost any nonlinear hyperbolic PDE solver).  Nonetheless,
- * it is much simpler than the alternatives!
- *
+ * solvers for problems with special initial data (so-called Riemann
+ * problems), nor even that we compute Jacobians of the flux
+ * functions.  The scheme is still somewhat complicated, partly
+ * because of the existence of a "limiter" (required for stability for
+ * almost any nonlinear hyperbolic PDE solver).  Nonetheless, it is
+ * much simpler than the alternatives!
+ * 
  * The Jiang-Tadmor scheme works by alternating between two staggered
  * grids.  We currently manage this implicitly: location (i,j) in the
  * solution
- *
+ * 
  * I note that Jiang and Tadmor use a different set of hyperbolic PDEs,
  * the Euler equations, instead of the shallow water equations.  If I've
  * done my job right, there should be no need to change the code below to
  * accomodate the Euler equations; the only thing that changes is the
  * implementation of the physics class.
- *
- * Ref:
- * http://www.cscamm.umd.edu/tadmor/pub/central-schemes/Jiang-Tadmor.SISSC-98.pdf
+ * 
+ * [jt]: http://www.cscamm.umd.edu/tadmor/pub/central-schemes/Jiang-Tadmor.SISSC-98.pdf
+ * 
+ * ## Interface
+ * 
+ * The `Central2D` solver class takes a *template argument* (`Physics`)
+ * that defines:
+ * 
+ *  - A type for numerical data (`real`)
+ *  - A type for solution and flux vectors in each cell (`vec`)
+ *  - A flux computation function (`flux(vec& F, vec& G, const vec& U)`)
+ *  - A wave speed computation function 
+ *    (`wave_speed(real& cx, real& cy, const vec& U)`).
+ * 
+ * The solver keeps arrays for the solution, flux values, derivatives
+ * of the solution and the fluxes, and the solution at the next time
+ * point.  We use the C++ `vector` class to manage storage for these
+ * arrays; but since we want to think of them as 2D arrays, we also
+ * provide convenience functions 
  */
+
 template <class Physics>
 class Central2D {
 public:
@@ -196,6 +276,7 @@ private:
 
 };
 
+//ldoc off
 
 /*
  * Apply periodic boundary conditions by copying ghost cell data
