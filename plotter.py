c24263c74b161c73eec4d226cff22333d942091b
sheroze1123 <mss385@cornell.edu>
2015-10-10 17:08:38 -0400

    Improved plotter function

--- plotter.py
+++ plotter.py
@@ -2,18 +2,26 @@ import csv
 import matplotlib.pyplot as plt
 from matplotlib import rc
 import numpy as np
+import sys
 
 rc('font',**{'family':'serif','serif':['Palatino']})
 rc('text', usetex=True)
 
+file_name = None
+
+if len(sys.argv) == 1:
+    file_name = 'timings'       
+elif len(sys.argv) == 2:
+    file_name = sys.argv[1]
+
 t = []
 
-with open('timings.csv', 'rb') as csvfile:
+with open(file_name + '.csv', 'rb') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
     for row in reader:
         t.append(float(row[0]))
 
 plt.plot(t)
-plt.savefig('timings.png')
+plt.savefig(file_name + '.png')
 plt.clf()
 plt.cla()
