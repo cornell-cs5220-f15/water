eafcd50525b68533f6e836287dc83b82a178b968
David Bindel <bindel@cs.cornell.edu>
2015-10-13 06:17:57 -0400

    Merged MinMod into main central2d class.

--- central2d.h
+++ central2d.h
@@ -52,6 +52,57 @@
  * function always takes an even number of time steps to ensure we end
  * up on the primary grid.
  *
+ * ## MinMod limiter
+ *
+ * Numerical methods for solving nonlinear wave equations are
+ * complicated by the fact that even with smooth initial data, a
+ * nonlinear wave can develop discontinuities (shocks) in finite time.
+ *
+ * This makes for interesting analysis, since a "strong" solution
+ * that satisfies the differential equation no longer makes sense at
+ * a shock -- instead, we have to come up with some mathematically
+ * and physically reasonable definition of a "weak" solution that
+ * satisfies the PDE away from the shock and satisfies some other
+ * condition (an entropy condition) at the shock.
+ *
+ * The presence of shocks also makes for interesting *numerical*
+ * analysis, because we need to be careful about employing numerical
+ * differentiation formulas that sample a discontinuous function at
+ * points on different sides of a shock.  Using such formulas naively
+ * usually causes the numerical method to become unstable.  A better
+ * method -- better even in the absence of shocks! -- is to consider
+ * multiple numerical differentiation formulas and use the highest
+ * order one that "looks reasonable" in the sense that it doesn't
+ * predict wildly larger slopes than the others.  Because these
+ * combined formulas *limit* the wild behavior of derivative estimates
+ * across a shock, we call them *limiters*.  With an appropriate limiter,
+ * we can construct methods that have high-order accuracy away from shocks
+ * and are at least first-order accurate close to a shock.  These are
+ * sometimes called *high-resolution* methods.
+ *
+ * The MinMod (minimum modulus) limiter is one example of a limiter.
+ * The MinMod limiter estimates the slope through points $f_-, f_0, f_+$
+ * (with the step $h$ scaled to 1) by
+ * $$
+ *   f' = \operatorname{minmod}((f_+-f_-)/2, \theta(f_+-f_0), \theta(f_0-f_-))
+ * $$
+ * where the minmod function returns the argument with smallest absolute
+ * value if all arguments have the same sign, and zero otherwise.
+ * Common choices of $\theta$ are $\theta = 1.0$ and $\theta = 2.0$.
+ *
+ * The minmod limiter *looks* like it should be expensive to computer,
+ * since superficially it seems to require a number of branches.
+ * We do something a little tricky, getting rid of the condition
+ * on the sign of the arguments using the `copysign` instruction.
+ * If the compiler does the "right" thing with `max` and `min`
+ * for floating point arguments (translating them to branch-free
+ * intrinsic operations), this implementation should be relatively fast.
+ *
+ * There are many other potential choices of limiters as well.  We'll
+ * stick with this one for the code, but you should feel free to
+ * experiment with others if you know what you're doing and think it
+ * will improve performance or accuracy.
+ *
  * ## Interface
  *
  * We want a clean separation between the physics, the solver,
@@ -63,8 +114,7 @@
  * at compile time, that describe separate classes to implement the
  * physics and the limiter.
  *
- * The `Central2D` solver class takes two template arguments:
- * `Physics` and `Limiter`.  For `Physics`, we expect the name of a class
+ * The `Central2D` solver class takes the template arguments `Physics`
  * that defines:
  *
  *  - A type for numerical data (`real`)
@@ -72,15 +122,6 @@
  *  - A flux computation function (`flux(vec& F, vec& G, const vec& U)`)
  *  - A wave speed computation function
  *    (`wave_speed(real& cx, real& cy, const vec& U)`).
- *
- * The `Limiter` argument is a type with a static function `limdiff`
- * with the signature
- *
- *         limdiff(fm, f0, fp)
- *
- * The semantics are that `fm`, `f0`, and `fp` are three successive
- * grid points in some direction, and the function returns an approximate
- * (scaled) derivative value from these points.
  *
  * The solver keeps arrays for the solution, flux values, derivatives
  * of the solution and the fluxes, and the solution at the next time
@@ -92,7 +133,7 @@
  * of the domain has index (0,0).
  */
 
-template <class Physics, class Limiter>
+template <class Physics>
 class Central2D {
 public:
     typedef typename Physics::real real;
@@ -177,6 +218,25 @@ private:
 
     real& uwrap(int k, int ix, int iy)  { return u_[ioffset(k,ix,iy)]; }
 
+    // Limiter computations
+    static constexpr real theta = 2.0;
+
+    // Branch-free computation of minmod of two numbers
+    static real xmin(real a, real b) {
+        using namespace std;
+        return ((copysign((real) 0.5, a) +
+                 copysign((real) 0.5, b)) *
+                min( abs(a), abs(b) ));
+    }
+
+    // Limited combined slope estimate
+    static real limdiff(real um, real u0, real up) {
+        real du1 = u0-um;         // Difference to left
+        real du2 = up-u0;         // Difference to right
+        real duc = 0.5*(du1+du2); // Centered difference
+        return xmin( theta*xmin(du1, du2), duc );
+    }
+
     // Stages of the main algorithm
     void apply_periodic();
     void limited_derivs();
@@ -202,8 +262,8 @@ private:
  * integers `p` and `q`.
  */
 
-template <class Physics, class Limiter>
-void Central2D<Physics, Limiter>::apply_periodic()
+template <class Physics>
+void Central2D<Physics>::apply_periodic()
 {
     // Copy data between right and left boundaries
     for (int k = 0; k < nfield; ++k) {
@@ -231,20 +291,20 @@ void Central2D<Physics, Limiter>::apply_periodic()
  * In order to maintain stability, we apply a limiter here.
  */
 
-template <class Physics, class Limiter>
-void Central2D<Physics, Limiter>::limited_derivs()
+template <class Physics>
+void Central2D<Physics>::limited_derivs()
 {
     for (int k = 0; k < nfield; ++k) {
         for (int iy = 1; iy < ny_all-1; ++iy)
             for (int ix = 1; ix < nx_all-1; ++ix) {
 
                 // x derivs
-                ux(k,ix,iy) = Limiter::limdiff(u(k,ix-1,iy), u(k,ix,iy), u(k,ix+1,iy));
-                fx(k,ix,iy) = Limiter::limdiff(f(k,ix-1,iy), f(k,ix,iy), f(k,ix+1,iy));
+                ux(k,ix,iy) = limdiff(u(k,ix-1,iy), u(k,ix,iy), u(k,ix+1,iy));
+                fx(k,ix,iy) = limdiff(f(k,ix-1,iy), f(k,ix,iy), f(k,ix+1,iy));
 
                 // y derivs
-                uy(k,ix,iy) = Limiter::limdiff(u(k,ix,iy-1), u(k,ix,iy), u(k,ix,iy+1));
-                gy(k,ix,iy) = Limiter::limdiff(g(k,ix,iy-1), g(k,ix,iy), g(k,ix,iy+1));
+                uy(k,ix,iy) = limdiff(u(k,ix,iy-1), u(k,ix,iy), u(k,ix,iy+1));
+                gy(k,ix,iy) = limdiff(g(k,ix,iy-1), g(k,ix,iy), g(k,ix,iy+1));
             }
     }
 }
@@ -272,8 +332,8 @@ void Central2D<Physics, Limiter>::limited_derivs()
  * indexing scheme.
  */
 
-template <class Physics, class Limiter>
-void Central2D<Physics, Limiter>::compute_step(int io, real dt)
+template <class Physics>
+void Central2D<Physics>::compute_step(int io, real dt)
 {
     real dtcdx2 = 0.5 * dt / dx;
     real dtcdy2 = 0.5 * dt / dy;
@@ -290,7 +350,6 @@ void Central2D<Physics, Limiter>::compute_step(int io, real dt)
                 v(k,ix,iy) = u(k,ix,iy) -
                     dtcdx2 * fx(k,ix,iy) -
                     dtcdy2 * gy(k,ix,iy);
-
             }
     }
 
@@ -341,8 +400,8 @@ void Central2D<Physics, Limiter>::compute_step(int io, real dt)
  * at the end lives on the main grid instead of the staggered grid.
  */
 
-template <class Physics, class Limiter>
-void Central2D<Physics, Limiter>::run(real tfinal)
+template <class Physics>
+void Central2D<Physics>::run(real tfinal)
 {
     bool done = false;
     real t = 0;
@@ -377,8 +436,8 @@ void Central2D<Physics, Limiter>::run(real tfinal)
  * of water heights).
  */
 
-template <class Physics, class Limiter>
-void Central2D<Physics, Limiter>::solution_check()
+template <class Physics>
+void Central2D<Physics>::solution_check()
 {
     using namespace std;
     real h_sum = 0, hu_sum = 0, hv_sum = 0;
--- ldriver.cc
+++ ldriver.cc
@@ -30,7 +30,7 @@ extern "C" {
  * limiter:
  */
 
-typedef Central2D< Shallow2D, MinMod<Shallow2D::real> > Sim;
+typedef Central2D<Shallow2D> Sim;
 
 
 /**
