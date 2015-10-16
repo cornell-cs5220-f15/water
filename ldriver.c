a4d5a89883d2bd1921ed7bc9d306eb21690934d0
David Bindel <bindel@cs.cornell.edu>
2015-10-15 22:09:08 -0400

    Enhanced timing output.

--- ldriver.c
+++ ldriver.c
@@ -215,32 +215,35 @@ int run_sim(lua_State* L)
     central2d_t* sim = central2d_init(w,h, nx,ny,
                                       3, shallow2d_flux, shallow2d_speed, cfl);
     lua_init_sim(L,sim);
-
     printf("%g %g %d %d %g %d %g\n", w, h, nx, ny, cfl, frames, ftime);
     FILE* viz = viz_open(fname, sim);
     solution_check(sim);
     viz_frame(viz, sim);
+
+    double tcompute = 0;
     for (int i = 0; i < frames; ++i) {
 #ifdef _OPENMP
         double t0 = omp_get_wtime();
         int nstep = central2d_run(sim, ftime);
         double t1 = omp_get_wtime();
         double elapsed = t1-t0;
-        printf("Time: %e (%e for %d steps)\n", elapsed, elapsed/nstep, nstep);
 #elif defined SYSTIME
         struct timeval t0, t1;
         gettimeofday(&t0, NULL);
         int nstep = central2d_run(sim, ftime);
         gettimeofday(&t1, NULL);
         double elapsed = (t1.tv_sec-t0.tv_sec) + (t1.tv_usec-t0.tv_usec)*1e-6;
-        printf("Time: %e (%e for %d steps)\n", elapsed, elapsed/nstep, nstep);
 #else
         int nstep = central2d_run(sim, ftime);
-        printf("Took %d steps\n", nstep);
+        double elapsed = 0;
 #endif
         solution_check(sim);
+        tcompute += elapsed;
+        printf("  Time: %e (%e for %d steps)\n", elapsed, elapsed/nstep, nstep);
         viz_frame(viz, sim);
     }
+    printf("Total compute time: %e\n", tcompute);
+
     central2d_free(sim);
     return 0;
 }
