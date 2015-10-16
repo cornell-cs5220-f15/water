05952726a39568cb1b938b89f264a332d6eff815
Michael Whittaker <mjw297@cornell.edu>
2015-10-15 23:34:24 -0400

    Removed comments from vec + minor spacing.

--- central2d_vec.h
+++ central2d_vec.h
@@ -6,92 +6,6 @@
 #include <cstdio>
 #include <cstdlib>
 
-//ldoc on
-/**
- * # Jiang-Tadmor central difference scheme
- *
- * [Jiang and Tadmor][jt] proposed a high-resolution finite difference
- * scheme for solving hyperbolic PDE systems in two space dimensions.
- * The method is particularly attractive because, unlike many other
- * methods in this space, it does not require that we write any
- * solvers for problems with special initial data (so-called Riemann
- * problems), nor even that we compute Jacobians of the flux
- * functions.
- *
- * While this code is based loosely on the Fortran code at the end of
- * Jiang and Tadmor's paper, we've written the current code to be
- * physics-agnostic (rather than hardwiring it to the shallow water
- * equations -- or the Euler equations in the Jiang-Tadmor paper).
- * If you're interested in the Euler equations, feel free to add your
- * own physics class to support them!
- *
- * [jt]: http://www.cscamm.umd.edu/tadmor/pub/central-schemes/Jiang-Tadmor.SISSC-98.pdf
- *
- * ## Staggered grids
- *
- * The Jiang-Tadmor scheme works by alternating between a main grid
- * and a staggered grid offset by half a step in each direction.
- * Understanding this is important, particularly if you want to apply
- * a domain decomposition method and batch time steps between
- * synchronization barriers in your parallel code!
- *
- * In even-numbered steps, the entry `u(i,j)` in the array of solution
- * values represents the average value of a cell centered at a point
- * $(x_i,y_j)$.  At the following odd-numbered step, the same entry
- * represents values for a cell centered at $(x_i + \Delta x/2, y_j +
- * \Delta y/2)$.  However, whenever we run a simulation, we always take
- * an even number of steps, so that outside the solver we can just think
- * about values on the main grid.  If `uold` and `unew` represent the
- * information at two successive *even* time steps (i.e. they represent
- * data on the same grid), then `unew(i,j)` depends indirectly on
- * `u(p,q)` for $i-3 \leq p \leq i+3$ and $j-3 \leq q \leq j+3$.
- *
- * We currently manage this implicitly: the arrays at even time steps
- * represent cell values on the main grid, and arrays at odd steps
- * represent cell values on the staggered grid.  Our main `run`
- * function always takes an even number of time steps to ensure we end
- * up on the primary grid.
- *
- * ## Interface
- *
- * We want a clean separation between the physics, the solver,
- * and the auxiliary limiter methods used by the solver.  At the same
- * time, we don't want to pay the overhead (mostly in terms of lost
- * optimization opportunities) for calling across an abstraction
- * barrier in the inner loops of our solver.  We can get around this
- * in C++ by providing the solver with *template arguments*, resolved
- * at compile time, that describe separate classes to implement the
- * physics and the limiter.
- *
- * The `Central2DVec` solver class takes two template arguments:
- * `Physics` and `Limiter`.  For `Physics`, we expect the name of a class
- * that defines:
- *
- *  - A type for numerical data (`real`)
- *  - A type for solution and flux vectors in each cell (`vec`)
- *  - A flux computation function (`flux(vec& F, vec& G, const vec& U)`)
- *  - A wave speed computation function
- *    (`wave_speed(real& cx, real& cy, const vec& U)`).
- *
- * The `Limiter` argument is a type with a static function `limdiff`
- * with the signature
- *
- *         limdiff(fm, f0, fp)
- *
- * The semantics are that `fm`, `f0`, and `fp` are three successive
- * grid points in some direction, and the function returns an approximate
- * (scaled) derivative value from these points.
- *
- * The solver keeps arrays for the solution, flux values, derivatives
- * of the solution and the fluxes, and the solution at the next time
- * point.  We use the C++ `vector` class to manage storage for these
- * arrays; but since we want to think of them as 2D arrays, we also
- * provide convenience functions to access them with multiple indices
- * (though we maintain C-style 0-based indexing).  The internal arrays
- * are padded with ghost cells; the ghost cell in the lower left corner
- * of the domain has index (0,0).
- */
-
 template <class Physics, class Limiter>
 class Central2DVec {
 public:
@@ -100,8 +14,8 @@ public:
     static constexpr int num_fields = Physics::num_fields;
 
     Central2DVec(real w, real h,     // Domain width / height
-              int nx, int ny,     // Number of cells in x/y (without ghosts)
-              real cfl = 0.45) :  // Max allowed CFL number
+                 int nx, int ny,     // Number of cells in x/y (without ghosts)
+                 real cfl = 0.45) :  // Max allowed CFL number
         nx(nx), ny(ny),
         nx_all(nx + 2*nghost),
         ny_all(ny + 2*nghost),
@@ -218,18 +132,6 @@ private:
     void compute_step(int io, real dt);
 };
 
-
-/**
- * ## Initialization
- *
- * Before starting the simulation, we need to be able to set the
- * initial conditions.  The `init` function does exactly this by
- * running a callback function at the center of each cell in order
- * to initialize the cell $U$ value.  For the purposes of this function,
- * cell $(i,j)$ is the subdomain
- * $[i \Delta x, (i+1) \Delta x] \times [j \Delta y, (j+1) \Delta y]$.
- */
-
 template <class Physics, class Limiter>
 template <typename F>
 void Central2DVec<Physics, Limiter>::init(F f) {
@@ -246,23 +148,6 @@ void Central2DVec<Physics, Limiter>::init(F f) {
         }
     }
 }
-
-/**
- * ## Time stepper implementation
- *
- * ### Boundary conditions
- *
- * In finite volume methods, boundary conditions are typically applied by
- * setting appropriate values in ghost cells.  For our framework, we will
- * apply periodic boundary conditions; that is, waves that exit one side
- * of the domain will enter from the other side.
- *
- * We apply the conditions by assuming that the cells with coordinates
- * `nghost <= ix <= nx+nghost` and `nghost <= iy <= ny+nghost` are
- * "canonical", and setting the values for all other cells `(ix,iy)`
- * to the corresponding canonical values `(ix+p*nx,iy+q*ny)` for some
- * integers `p` and `q`.
- */
 
 template <class Physics, class Limiter>
 void Central2DVec<Physics, Limiter>::apply_periodic() {
@@ -289,17 +174,6 @@ void Central2DVec<Physics, Limiter>::apply_periodic() {
     }
 }
 
-
-/**
- * ### Initial flux and speed computations
- *
- * At the start of each time step, we need the flux values at
- * cell centers (to advance the numerical method) and a bound
- * on the wave speeds in the $x$ and $y$ directions (so that
- * we can choose a time step that respects the specified upper
- * bound on the CFL number).
- */
-
 template <class Physics, class Limiter>
 void Central2DVec<Physics, Limiter>::compute_fg_speeds(real& cx_, real& cy_)
 {
@@ -321,14 +195,6 @@ void Central2DVec<Physics, Limiter>::compute_fg_speeds(real& cx_, real& cy_)
     cy_ = cy;
 }
 
-/**
- * ### Derivatives with limiters
- *
- * In order to advance the time step, we also need to estimate
- * derivatives of the fluxes and the solution values at each cell.
- * In order to maintain stability, we apply a limiter here.
- */
-
 template <class Physics, class Limiter>
 void Central2DVec<Physics, Limiter>::limited_derivs() {
     using L = Limiter;
@@ -346,29 +212,6 @@ void Central2DVec<Physics, Limiter>::limited_derivs() {
         }
     }
 }
-
-
-/**
- * ### Advancing a time step
- *
- * Take one step of the numerical scheme.  This consists of two pieces:
- * a first-order corrector computed at a half time step, which is used
- * to obtain new $F$ and $G$ values; and a corrector step that computes
- * the solution at the full step.  For full details, we refer to the
- * [Jiang and Tadmor paper][jt].
- *
- * The `compute_step` function takes two arguments: the `io` flag
- * which is the time step modulo 2 (0 if even, 1 if odd); and the `dt`
- * flag, which actually determines the time step length.  We need
- * to know the even-vs-odd distinction because the Jiang-Tadmor
- * scheme alternates between a primary grid (on even steps) and a
- * staggered grid (on odd steps).  This means that the data at $(i,j)$
- * in an even step and the data at $(i,j)$ in an odd step represent
- * values at different locations in space, offset by half a space step
- * in each direction.  Every other step, we shift things back by one
- * mesh cell in each direction, essentially resetting to the primary
- * indexing scheme.
- */
 
 template <class Physics, class Limiter>
 void Central2DVec<Physics, Limiter>::compute_step(int io, real dt)
@@ -423,21 +266,6 @@ void Central2DVec<Physics, Limiter>::compute_step(int io, real dt)
     }
 }
 
-
-/**
- * ### Advance time
- *
- * The `run` method advances from time 0 (initial conditions) to time
- * `tfinal`.  Note that `run` can be called repeatedly; for example,
- * we might want to advance for a period of time, write out a picture,
- * advance more, and write another picture.  In this sense, `tfinal`
- * should be interpreted as an offset from the time represented by
- * the simulator at the start of the call, rather than as an absolute time.
- *
- * We always take an even number of steps so that the solution
- * at the end lives on the main grid instead of the staggered grid.
- */
-
 template <class Physics, class Limiter>
 void Central2DVec<Physics, Limiter>::run(real tfinal)
 {
@@ -462,18 +290,6 @@ void Central2DVec<Physics, Limiter>::run(real tfinal)
         }
     }
 }
-
-/**
- * ### Diagnostics
- *
- * The numerical method is supposed to preserve (up to rounding
- * errors) the total volume of water in the domain and the total
- * momentum.  Ideally, we should also not see negative water heights,
- * since that will cause the system of equations to blow up.  For
- * debugging convenience, we'll plan to periodically print diagnostic
- * information about these conserved quantities (and about the range
- * of water heights).
- */
 
 template <class Physics, class Limiter>
 void Central2DVec<Physics, Limiter>::solution_check()
@@ -501,5 +317,4 @@ void Central2DVec<Physics, Limiter>::solution_check()
            h_sum, hu_sum, hv_sum, hmin, hmax);
 }
 
-//ldoc off
 #endif /* CENTRAL2D_VEC_H*/
