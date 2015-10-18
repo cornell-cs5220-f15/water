d427abf2ae7a10e3b77f57fa5e731823e8df2439
Edward Tremel <high.order.bits@gmail.com>
2015-10-18 19:01:15 -0400

    Added timing scripts to run the simulation repeatedly for different values of n and record the average frame time.

---
+++ make_csv.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ -z $1 ]; then
+	echo "Error: No directory provided"
+	return
+fi
+
+cd $1
+touch timing.csv
+namepattern='run([0-9]+).out'
+for file in `ls -v *.out`; do
+	if [[ $file =~ $namepattern ]]; then
+		gridsize=${BASH_REMATCH[1]}
+		frametime=`tail -n 1 $file`
+		echo "$gridsize,$frametime" >> timing.csv
+	fi
+done
+
