bfbe291eeaf88d59f434e7c570672607561abcc0
Unmukt Gupta <ug36@en-cs-totient-01.coecis.cornell.edu>
2015-10-19 23:40:22 -0400

    AVX compute_step

--- vec_driver.cc
+++ vec_driver.cc
@@ -137,7 +137,9 @@ int main(int argc, char** argv)
     } else {
         fprintf(stderr, "Unknown initial conditions\n");
     }
-    
+ 
+
+    double t0 = omp_get_wtime();   
     Sim sim(width,width, nx,nx);
     SimViz<Sim> viz(fname.c_str(), sim);
     sim.init(icfun);
@@ -145,14 +147,14 @@ int main(int argc, char** argv)
     viz.write_frame();
     for (int i = 0; i < frames; ++i) {
 #ifdef _OPENMP
-        double t0 = omp_get_wtime();
         sim.run(ftime);
-        double t1 = omp_get_wtime();
-        printf("Time: %e\n", t1-t0);
 #else
         sim.run(ftime);
 #endif
         sim.solution_check();
         viz.write_frame();
     }
+        double t1 = omp_get_wtime();
+        printf("Time: %e\n", t1-t0);
+
 }
