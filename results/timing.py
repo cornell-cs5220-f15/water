96d675b20c05c0dfefea5a845299befd4f173e10
Patrick Cao <pxc2@cornell.edu>
2015-10-20 19:17:42 -0400

    Fixed timing script

--- results/timing.py
+++ results/timing.py
@@ -4,14 +4,13 @@ import time
 n= [300,600,900,1200,1500,2400,3600]
 procs= [1,4,9,16,25,36]
 rounds= [1]
-dirp = "results"
 
 indx = 0
 
 for np in procs:
   for nr in rounds:
     for ni in n:
-      fname = "{}/{}.pbs".format(dirp,indx)
+      fname = "{}.pbs".format(indx)
       out = open(fname,'w')
       s = '''#!/bin/sh -l
 #PBS -l nodes=1:ppn=24
@@ -19,12 +18,16 @@ for np in procs:
 #PBS -N lshallow
 #PBS -j oe
 module load cs5220
-cd $PBS_O_WORKDIR'''
+cd $PBS_O_WORKDIR
+'''
       out.write(s)
 
-      line = "./lshallow tests.lua dam {} {} {}\n".format(ni, np, nr)
+      line = "./../lshallow ../tests.lua dam {} {} {}\n".format(ni, np, nr)
       out.write(line)
       indx = indx + 1
       out.close()
+      if ni >= 1500:
+        time.sleep(20)
+      else:
+        time.sleep(1)
       os.system("qsub {}".format(fname))
-      time.sleep(10)        
