66c21fb6f75b3d058be7735c405590d81acbd257
Ben Shulman <shulman.benjamin@gmail.com>
2015-10-28 11:01:57 -0400

    added more data

--- results/generate_graphs.py
+++ results/generate_graphs.py
@@ -10,7 +10,7 @@ def average_results(data, baseline=False):
         if not baseline:
             for p in np.unique(temp['p']):
                 temp2 = temp[temp['p'] == p]
-                output.append((n, p, np.mean(temp2['time']))) #TODO: remove 1
+                output.append((n, p, np.mean(temp2['time'])))
         else:
             output.append((n, np.mean(temp['time'])))
     if baseline:
@@ -20,14 +20,13 @@ def average_results(data, baseline=False):
 
 
 
-results = np.genfromtxt('mic1/results.csv', delimiter=',', names=True)
+results = np.genfromtxt('results.csv', delimiter=',', names=True)
 results = average_results(results)
 baseline = np.genfromtxt('baseline_360.csv', delimiter=',', names=True)
 baseline = average_results(baseline, baseline=True)
 baseline.sort(order=['n'])
 N = baseline['n']
 baseline_y = baseline['time']
-# colors = ['ro-', 'g+-', 'b*-', 'y^-', 'ks-', 'm*-', 'c']
 colors = ['red', 'green', 'blue', 'yellow', 'black', 'magenta', 'cyan', 'brown', 'orange', 'gray', 'purple', 'pink']
 # strong scaling
 for i, n in enumerate(N):
@@ -40,7 +39,7 @@ plt.legend(bbox_to_anchor=(1.12,1),
           ncol=1)
 plt.xlabel('Threads')
 plt.ylabel('Speedup')
-plt.savefig('../report/mic_strong_bindel_baseline.pdf')
+plt.savefig('strong.pdf')
 plt.close()
 
 # weak scaling
@@ -58,5 +57,5 @@ plt.legend(bbox_to_anchor=(1.12,1),
           ncol=1)
 plt.xlabel('Threads')
 plt.ylabel('Speedup')
-plt.savefig('../report/mic_weak_bindel_baseline.pdf')
+plt.savefig('weak.pdf')
 
