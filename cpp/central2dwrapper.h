335a39b6f901f480574fc2951e4e3466164544c9
nimit <nss66@en-cs-totient-01.coecis.cornell.edu>
2015-10-29 18:25:53 -0400

    integrated both methods

--- cpp/central2dwrapper.h
+++ cpp/central2dwrapper.h
@@ -1,6 +1,8 @@
 #ifndef CENTRAL2DWRAPPER_H
 #define CENTRAL2DWRAPPER_H
 
+#define DEBUG 0
+
 #include <cstdio>
 #include <cmath>
 #include <cassert>
@@ -23,9 +25,10 @@ public:
 		nx(nx), ny(ny),
 		nx_all(nx + 2*nghost),
 		ny_all(ny + 2*nghost),
-		cfl(cfl),
-		u_ (new std::vector<vec>(nx_all * ny_all)),
-		v_ (new std::vector<vec>(nx_all * ny_all)) {}
+		cfl(cfl) { u_ = (float **)malloc(sizeof(float*));
+				   v_ = (float **)malloc(sizeof(float*));
+				   *u_ = (float *)malloc(sizeof(float) * nx_all * ny_all * 3);
+				   *v_ = (float *)malloc(sizeof(float) * nx_all * ny_all * 3); }
 
 	~Central2DWrapper() {
 		delete(u_);
@@ -47,12 +50,12 @@ public:
     int ysize() const { return ny; }
     
     // Read / write elements of simulation state
-    vec&       operator()(int i, int j) {
-        return (*u_)[offset(i+nghost,j+nghost)];
-    }
-    
-    const vec& operator()(int i, int j) const {
-        return (*u_)[offset(i+nghost,j+nghost)];
+    const vec operator()(int i, int j) const {
+        vec vr;
+        vr[0] = (*u_)[offset(i+nghost,j+nghost,0)];
+        vr[1] = (*u_)[offset(i+nghost,j+nghost,1)];
+        vr[2] = (*u_)[offset(i+nghost,j+nghost,2)];
+        return vr;
     }
 
 private:
@@ -68,23 +71,23 @@ private:
 
 	//Note that these are now stored on the heap so that we can swap
 	//the pointers to them between steps, instead of copying from v to u
-    std::vector<vec>* u_;            // Solution values
-    std::vector<vec>* v_;            // Solution values at next step
+    float** u_;            // Solution values
+    float** v_;            // Solution values at next step
 
     // Array accessor functions
 
-    int offset(int ix, int iy) const { return iy*nx_all+ix; }
+    inline int offset(int ix, int iy, short d) const { return ((d*ny_all)+iy)*nx_all+ix; }
 
-    vec& u(int ix, int iy)    { return (*u_)[offset(ix,iy)]; }
-    vec& v(int ix, int iy)    { return (*v_)[offset(ix,iy)]; }
+    inline float& u(int ix, int iy, short d)    { return (*u_)[offset(ix,iy,d)]; }
+    inline float& v(int ix, int iy, short d)    { return (*v_)[offset(ix,iy,d)]; }
 
     // Wrapped accessor (periodic BC)
-    int ioffset(int ix, int iy) {
-        return offset( (ix+nx-nghost) % nx + nghost,
-                       (iy+ny-nghost) % ny + nghost );
+    int ioffset(int ix, int iy, short d) {
+        return offset((ix+nx-nghost) % nx + nghost,
+                       (iy+ny-nghost) % ny + nghost, d );
     }
 
-    vec& uwrap(int ix, int iy)  { return (*u_)[ioffset(ix,iy)]; }
+    inline float& uwrap(int ix, int iy, short d)  { return (*u_)[ioffset(ix,iy,d)]; }
 
 	// Compute the wave speeds and choose a timestep, without advancing time
 	real compute_current_dt();
@@ -92,8 +95,6 @@ private:
 	//The only simulation function needed at the wrapper level
     void apply_periodic();
 
-	void check_copyin(Central2D<Physics, Limiter>& wrappedSim, std::vector<vec>* large_u);
-	void check_copyout(Central2D<Physics, Limiter>& wrappedSim, std::vector<vec>* large_v);
 };
 
 /**
@@ -111,9 +112,19 @@ template <class Physics, class Limiter>
 template <typename F>
 void Central2DWrapper<Physics, Limiter>::init(F f)
 {
-    for (int iy = 0; iy < ny; ++iy)
-        for (int ix = 0; ix < nx; ++ix)
-            f(u(nghost+ix,nghost+iy), (ix+0.5)*dx, (iy+0.5)*dy);
+    const int a=nx,b=ny;
+    for (int iy = 0; iy < b; ++iy) {
+        for (int ix = 0; ix < a; ++ix) {
+            vec vr; // Function f requires a vec, not a single float
+            vr[0] = u(nghost+ix,nghost+iy,0);
+            vr[1] = u(nghost+ix,nghost+iy,1);
+            vr[2] = u(nghost+ix,nghost+iy,2);
+            f(vr, (ix+0.5)*dx, (iy+0.5)*dy);
+            u(nghost+ix,nghost+iy,0) = vr[0];
+            u(nghost+ix,nghost+iy,1) = vr[1];
+            u(nghost+ix,nghost+iy,2) = vr[2]; // write out result to array
+        }
+    }
 }
 
 /**
@@ -137,18 +148,22 @@ template <class Physics, class Limiter>
 void Central2DWrapper<Physics, Limiter>::apply_periodic()
 {
     // Copy data between right and left boundaries
-    for (int iy = 0; iy < ny_all; ++iy)
+    for (short d = 0; d < 3; d++) {
         for (int ix = 0; ix < nghost; ++ix) {
-            u(ix,          iy) = uwrap(ix,          iy);
-            u(nx+nghost+ix,iy) = uwrap(nx+nghost+ix,iy);
+	    #pragma ivdep // vectorize this loop (ignore "dependencies")
+            for (int iy = 0; iy < ny_all; ++iy) {
+                u(ix,          iy,d) = uwrap(ix,          iy,d);
+                u(nx+nghost+ix,iy,d) = uwrap(nx+nghost+ix,iy,d);
+            }
         }
-
-    // Copy data between top and bottom boundaries
-    for (int ix = 0; ix < nx_all; ++ix)
         for (int iy = 0; iy < nghost; ++iy) {
-            u(ix,          iy) = uwrap(ix,          iy);
-            u(ix,ny+nghost+iy) = uwrap(ix,ny+nghost+iy);
+            #pragma ivdep
+            for (int ix = 0; ix < nx_all; ++ix) {
+                u(ix,          iy,d) = uwrap(ix,          iy,d);
+                u(ix,ny+nghost+iy,d) = uwrap(ix,ny+nghost+iy,d);
+            }
         }
+    }
 }
 
 template <class Physics, class Limiter>
@@ -160,7 +175,8 @@ Central2DWrapper<Physics, Limiter>::real Central2DWrapper<Physics, Limiter>::com
     for (int iy = 0; iy < ny_all; ++iy)
         for (int ix = 0; ix < nx_all; ++ix) {
             real cell_cx, cell_cy;
-            Physics::wave_speed(cell_cx, cell_cy, u(ix,iy));
+            Physics::wave_speed(cell_cx, cell_cy, u(ix,iy,0),
+                                u(ix,iy,1), u(ix,iy,2));
             cx = max(cx, cell_cx);
             cy = max(cy, cell_cy);
         }
@@ -168,48 +184,20 @@ Central2DWrapper<Physics, Limiter>::real Central2DWrapper<Physics, Limiter>::com
 	return dt;
 }
 
-//DEBUG: Only works when there's a single thread!
-template <class Physics, class Limiter>
-void Central2DWrapper<Physics, Limiter>::check_copyin(Central2D<Physics, Limiter>& wrappedSim, std::vector<vec>* large_u) 
-{
-	for(int x = 0; x < nx_all; x++) {
-		for(int y = 0; y < ny_all; y++) {
-			if(wrappedSim.u(x,y) != (*large_u)[offset(x, y)]) {
-				printf("Cell (%d, %d) was not equal after copy-in! Central2DWrapper had <%f, %f, %f> but wrapped Central2D had <%f, %f, %f>\n", x, y, (*large_u)[offset(x, y)][0], (*large_u)[offset(x, y)][1], (*large_u)[offset(x, y)][2], wrappedSim.u(x,y)[0], wrappedSim.u(x,y)[1], wrappedSim.u(x,y)[2]);
-			}
-		}
-	}
-}
-
-//DEBUG: Only works when there's a single thread!
-template <class Physics, class Limiter>
-void Central2DWrapper<Physics, Limiter>::check_copyout(Central2D<Physics, Limiter>& wrappedSim, std::vector<vec>* large_v) 
-{
-	for(int x = 0; x < nx; x++) {
-		for(int y = 0; y < ny; y++) {
-			if(wrappedSim.u(x+nghost,y+nghost) != (*large_v)[offset(x+nghost, y+nghost)]) {
-				printf("Cell (%d, %d) was not equal after copy-out! Central2DWrapper had <%f, %f, %f> but wrapped Central2D had <%f, %f, %f>\n", x+nghost, y+nghost, (*large_v)[offset(x+nghost, y+nghost)][0], (*large_v)[offset(x+nghost, y+nghost)][1], (*large_v)[offset(x+nghost, y+nghost)][2], wrappedSim.u(x+nghost,y+nghost)[0], wrappedSim.u(x+nghost,y+nghost)[1], wrappedSim.u(x+nghost,y+nghost)[2]);
-				return;
-			}
-		}
-	}
-}
-
 template <class Physics, class Limiter>
 void Central2DWrapper<Physics, Limiter>::run(const real tfinal)
 {
 	typedef Central2D<Physics, Limiter> Central2D_t;
 	//OpenMP won't allow you to share member variables, so copy the pointers to u and v into local variables
-	std::vector<vec>* large_u = u_;
-	std::vector<vec>* large_v = v_;
+	float** large_u = u_;
+	float** large_v = v_;
 	//Shared arrays for combining maximum dt values, but can't be initialized until we know the number of threads
 	std::vector<real>* local_dts_curr = new std::vector<real>();
 	std::vector<real>* local_dts_next = new std::vector<real>();
 	//DEBUG
-	omp_set_num_threads(1);
+	//omp_set_num_threads(1);
 	#pragma omp parallel firstprivate(tfinal, large_u, large_v, local_dts_curr,local_dts_next) default(none) 
 	{
-//		printf("Thread sees Central2DWrapper's nx = %d, ny = %d, nx_all = %d, ny_all = %d \n", nx, ny, nx_all, ny_all);
 		const int nthreads = omp_get_num_threads();
 		//Now that we know how many threads there are, split the board into
 		//approximately that many blocks. It would be nice if we could nicely 
@@ -235,7 +223,7 @@ void Central2DWrapper<Physics, Limiter>::run(const real tfinal)
 		std::vector<Central2D_t> blockSims;
 		const int my_numblocks = blockspp + 
 			(omp_get_thread_num() == nthreads-1 && one_extra ? 1 : 0);
-		//DEBUG
+		#if DEBUG
 		#pragma omp single 
 		{
 			printf("nthreads = %d\n", nthreads);
@@ -243,6 +231,7 @@ void Central2DWrapper<Physics, Limiter>::run(const real tfinal)
 			printf("blockspp = %d\n", blockspp);
 			printf("one_extra = %d\n", one_extra);
 		}
+                #endif
 		for(int b = 0; b < my_numblocks; b++) {
 			//Blocks are counted in row-major order across the grid
 			const int blocknum = b;
@@ -268,10 +257,12 @@ void Central2DWrapper<Physics, Limiter>::run(const real tfinal)
 				(*local_dts_curr)[i] = init_dt;
 		}
 		//Now we can start the actual per-thread computation
+		#if DEBUG
 		#pragma omp critical
 		{
 			printf("Thread %d has %d blocks of size %d x %d \n", omp_get_thread_num(), my_numblocks, bwidth, bheight);
 		}
+                #endif
 		real curtime = 0;
 		real dt;
 		bool done = false;
@@ -295,47 +286,47 @@ void Central2DWrapper<Physics, Limiter>::run(const real tfinal)
 				const int blockrow = blocknum / nblocksx;
 				const int blockcol = blocknum % nblocksx;
 				//Copy this block's data in from u, offsetting pointers by u's ghost cells
-				blockSims[b].init_as_subdomain(*large_u, nx_all, 
+				blockSims[b].init_as_subdomain(*large_u, nx_all, ny_all,
 						nghost + blockcol * bwidth, nghost + blockrow * bheight);
 				//Advance two timesteps locally, and put the new dt in dts_next
 				(*local_dts_next)[blocknum] = blockSims[b].take_timestep_pair(dt);
 				//Copy the results back out to v, so it can happen concurrently with reads from u
-				blockSims[b].copy_results_out(*large_v, nx_all,
+				blockSims[b].copy_results_out(*large_v, nx_all, ny_all,
 						nghost + blockcol * bwidth, nghost + blockrow * bheight);
 				//If this block is on an edge, also copy out ghost cells
     			if(blockcol == 0) {
-    				blockSims[b].copy_vert_ghosts(*large_v, nx_all, nx,
+    				blockSims[b].copy_vert_ghosts(*large_v, nx_all, ny_all, nx,
     						nghost + blockrow * bheight, false); 
     			}
     			//This must be checked in sequence, not with ||, because there might be only one column of blocks
     			if(blockcol == nblocksx-1) {
-    				blockSims[b].copy_vert_ghosts(*large_v, nx_all, nx,
+    				blockSims[b].copy_vert_ghosts(*large_v, nx_all, ny_all, nx,
     						nghost + blockrow * bheight, true); 
     			}
 				//Similarly, both might execute if there's only one row of blocks
     			if(blockrow == 0) {
-    				blockSims[b].copy_horiz_ghosts(*large_v, nx_all, ny,
+    				blockSims[b].copy_horiz_ghosts(*large_v, nx_all, ny_all, ny,
     						nghost + blockcol * bwidth, false); 
     			}
     			if(blockrow == nblocksy-1) {
-    				blockSims[b].copy_horiz_ghosts(*large_v, nx_all, ny,
+    				blockSims[b].copy_horiz_ghosts(*large_v, nx_all, ny_all, ny,
     						nghost + blockcol * bwidth, true); 
     			}
 				//Corner cases (literally)
 				if(blockrow == 0 && blockcol == 0) {
-					blockSims[b].copy_corner_ghosts(*large_v, nx_all, 
+					blockSims[b].copy_corner_ghosts(*large_v, nx_all, ny_all,
 							ny, nx, Corner::Southeast);
 				}
 				if(blockrow == 0 && blockcol == nblocksx-1) {
-					blockSims[b].copy_corner_ghosts(*large_v, nx_all, 
+					blockSims[b].copy_corner_ghosts(*large_v, nx_all, ny_all,
 							ny, nx, Corner::Southwest);
 				}
 				if(blockrow == nblocksy-1 && blockcol == 0) {
-					blockSims[b].copy_corner_ghosts(*large_v, nx_all,
+					blockSims[b].copy_corner_ghosts(*large_v, nx_all, ny_all,
 							ny, nx, Corner::Northeast);
 				}
 				if(blockrow == nblocksy-1 && blockcol == nblocksx-1) {
-					blockSims[b].copy_corner_ghosts(*large_v, nx_all,
+					blockSims[b].copy_corner_ghosts(*large_v, nx_all, ny_all,
 							ny, nx, Corner::Northwest);
 				}
 			}
@@ -370,18 +361,17 @@ void Central2DWrapper<Physics, Limiter>::solution_check()
 {
     using namespace std;
     real h_sum = 0, hu_sum = 0, hv_sum = 0;
-    real hmin = u(nghost,nghost)[0];
+    real hmin = u(nghost,nghost, 0);
     real hmax = hmin;
     for (int j = nghost; j < ny+nghost; ++j)
         for (int i = nghost; i < nx+nghost; ++i) {
-            vec& uij = u(i,j);
-            real h = uij[0];
+            real h = u(i, j, 0); // change these in order to typecheck
             h_sum += h;
-            hu_sum += uij[1];
-            hv_sum += uij[2];
+            hu_sum += u(i, j, 1);
+            hv_sum += u(i, j, 2);
             hmax = max(h, hmax);
             hmin = min(h, hmin);
-            assert( h > 0) ;
+            assert (h > 0) ;
         }
     real cell_area = dx*dy;
     h_sum *= cell_area;
